05d258c72974512cbcfcd0b322a2cd4a20f2ffce
Cédric Tellier <cedric.tellier25@gmail.com>
2026-05-03 00:21:13 +0200

    Updates

--- _next/static/chunks/pages/index-c70e186eff8e7423.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{6760:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(1815)}])}},_=>{_.O(0,[815,636,593,792],()=>_(_.s=6760)),_N_E=_.O()}]);
